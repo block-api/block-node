ca640e7d4d61dd16976d0d4700a7b0219161b085
mac <9588532+t3-mac@users.noreply.github.com>
2022-10-26 02:14:31 +0800

    moved parsing pocket with data to network package atm, it should not be in transporter

--- network/network.go
+++ network/network.go
@@ -6,9 +6,11 @@ import (
 	"github.com/block-api/block-node/db"
 	"github.com/block-api/block-node/log"
 	"github.com/block-api/block-node/transporter"
+	"github.com/block-api/block-node/utils"
 )
 
 type Network struct {
+	nodeID      utils.NodeID
 	db          *db.Database
 	transporter transporter.Transporter
 }
@@ -37,8 +39,20 @@ func (n *Network) Send(pocket transporter.Pocket[[]byte]) {
 	}
 }
 
-func (n *Network) Receive(pocket transporter.Pocket[[]byte]) {
-	log.Debug("Network.Receive Pocket[[]byte]")
+func (n *Network) Receive(payload []byte) {
+	log.Debug("Network.Receive []byte")
+
+	pocket := transporter.Pocket[[]byte]{}
+	err := json.Unmarshal([]byte(payload), &pocket)
+	if err != nil {
+		log.Warning(err.Error())
+		return
+	}
+
+	if (pocket.FromID == n.nodeID) || (pocket.TargetID != "" && pocket.TargetID != n.nodeID) {
+		log.Debug("skip self/not target")
+		return
+	}
 
 	if pocket.Channel == transporter.ChanDiscovery {
 		discoveryPayload, err := decodePayload[transporter.PayloadDiscovery](&pocket)
@@ -59,8 +73,9 @@ func (n *Network) Receive(pocket transporter.Pocket[[]byte]) {
 	}
 }
 
-func NewNetwork(transporter transporter.Transporter, db *db.Database) Network {
+func NewNetwork(nodeID utils.NodeID, transporter transporter.Transporter, db *db.Database) Network {
 	return Network{
+		nodeID:      nodeID,
 		db:          db,
 		transporter: transporter,
 	}
