4ed7c4a8269d53a9d6baebdd53ceb91d153e0c65
mac <9588532+t3-mac@users.noreply.github.com>
2022-10-25 19:32:09 +0800

    init traffic manager, updates in types

--- utils/types.go
+++ utils/types.go
@@ -1,19 +1,29 @@
 package utils
 
 type (
-	NodeID    string
-	NodeName  string
-	BlockName string
+	NodeID          string
+	NodeName        string
+	NodeVersionName string
+	BlockName       string
+	ActionName      string
 )
 
-func (name NodeID) String() string {
-	return string(name)
+func (nodeID NodeID) String() string {
+	return string(nodeID)
 }
 
 func (name NodeName) String() string {
 	return string(name)
 }
 
+func (name NodeVersionName) String() string {
+	return string(name)
+}
+
 func (name BlockName) String() string {
+	return string(name)
+}
+
+func (name ActionName) String() string {
 	return string(name)
 }
