df851157ff6f2383ce1a2a1155ed772dd39fe4a1
mac <9588532+t3-mac@users.noreply.github.com>
2022-10-27 12:56:17 +0800

    update node network topology on connect/disconnect of other nodes

--- network/process_discovery.go
+++ network/process_discovery.go
@@ -3,10 +3,27 @@ package network
 import (
 	"fmt"
 
+	"github.com/block-api/block-node/log"
 	"github.com/block-api/block-node/transporter"
 )
 
 func (n *Network) ProcessPocketDiscovery(pocket transporter.Pocket[transporter.PayloadDiscovery]) {
 
-	fmt.Println(pocket)
+	if pocket.Payload.Event == transporter.EventConnected {
+		// Add to traffic Manager
+		for blockName, actions := range pocket.Payload.Blocks {
+			err := n.trafficManager.AddDestination(pocket.FromID, pocket.VersionName, blockName, actions)
+			if err != nil {
+				log.Warning(err.Error())
+				continue
+			}
+		}
+		// return
+	}
+
+	if pocket.Payload.Event == transporter.EventDisconnected {
+		n.trafficManager.RemoveNodeID(pocket.FromID)
+	}
+
+	fmt.Println(n.trafficManager.Destinations())
 }
